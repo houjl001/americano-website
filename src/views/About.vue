c25784a36588d41f62a4f31790c43164da99f568
houjunlei <houjunlei@cmos.chinamobile.com>
2018-11-08 09:43:23 +0800

    初始化仓库

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <h1>{{result}}</h1>
+    <div class="button" @click="add">add</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: 123
+    };
+  },
+  created() {
+    //console.log(this);
+  },
+  mounted() {
+    //console.log(this);
+    this.printThis();
+  },
+  computed: {
+    result() {
+      return this.data * this.data;
+    }
+  },
+  methods: {
+    printThis() {
+      console.log(this);
+    },
+    add() {
+      this.data++;
+    },
+  }
+}
+</script>
+<style lang="scss" scoped>
+.button {
+  width: 40px;
+  height: 20px;
+  background-color: aquamarine;
+}
+</style>
+
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
+  outputDir: 'server/dist',
+}
