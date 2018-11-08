88f252bf793bd40b825f05f88ccfe0010b6dbe98
houjunlei <houjunlei@cmos.chinamobile.com>
2018-11-08 15:13:10 +0800

    updata

---
+++ server/config/index.js
@@ -0,0 +1,6 @@
+import { merge } from 'lodash';
+
+const config = {
+  context_path: 'website',
+  api_path: 'api'
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   // outputDir: 在npm run build时 生成文件的目录 type:string, default:'dist'
   outputDir: 'server/dist',
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
 }
